45659dcf582148266aec6df4a1acbee1a0ec600b
bohdanka194 <bohdanka194@gmail.com>
2018-04-12 09:44:34 +0300

    'commit'

--- CubeOpenGLApp/Program.cs
+++ CubeOpenGLApp/Program.cs
@@ -10,6 +10,8 @@ namespace CubeOpenGLApp
     {
         static void Main(string[] args)
         {
+            OpenTK.GameWindow window = new OpenTK.GameWindow(800,600);
+            window.Run(1.0/60.0);
         }
     }
 }
