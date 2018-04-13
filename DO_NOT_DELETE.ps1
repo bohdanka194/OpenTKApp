57fe986502d4d226b8b34c94a16b64b03e67e89c
Drru97 <oleg.dystak@gmail.com>
2018-04-13 11:12:50 +0300

    Add very useful powershell script.

---
+++ DO_NOT_DELETE.ps1
@@ -0,0 +1,18 @@
+$name = "Oleh Dutsiak"
+$daylight = 9;
+$evening = 18;
+$night = 22;
+
+$hour = Get-Date -UFormat "%H"
+$greeting = ""
+
+Switch($hour) 
+{
+    { $hour -le $daylight -and $hour -gt $night } { $greeting = "Good night"; Break; }
+    { $hour -le $evening -and $hour -gt $daylight } { $greeting = "Good afternoon"; Break; }
+    { $hour -le $night -and $hour -gt $evening } { $greeting = "Good evening"; Break; }
+    Default { $greeting = "Good afternoon" }
+}
+
+$greeting += ", $name"
+Write-Host $greeting
