eb97dafe0c607b29824f4803950e3c8b0c8b1018
bohdanka194 <bohdanka194@gmail.com>
2018-04-13 13:13:22 +0300

    add new triangle

--- CubeOpenGLApp/Game.cs
+++ CubeOpenGLApp/Game.cs
@@ -21,30 +21,40 @@ namespace CubeOpenGLApp
             window.UpdateFrame += window_UpdateFrame;
             window.RenderFrame += window_RenderFrame;
         }
+
         void window_Load(object sender, EventArgs args)
         {
-            GL.ClearColor(Color.CornflowerBlue);
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-
-            window.SwapBuffers();
-            //throw new NotImplementedException();
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
         }
+
         void window_UpdateFrame(object sender, FrameEventArgs args)
         {
             //throw new NotImplementedException();
         }
+
         void window_RenderFrame(object sender, FrameEventArgs args)
         {
             GL.ClearColor(Color.CornflowerBlue);
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
             GL.Begin(PrimitiveType.Triangles);
+            GL.Color4(Color.Red);
             GL.Vertex2(0, 0);
+            GL.Color3(Color.Green);
             GL.Vertex2(0.8, 0);
+            GL.Color3(Color.Yellow);
             GL.Vertex2(0, 0.8);
+
+            GL.Color4(1f, 1f, 1f, 0.5f);
+            GL.Vertex2(-0.25f, 0.8f);
+            GL.Vertex2(0.8f, -0.25f);
+            GL.Vertex2(-0.25f, -0.25f);
+
+
+
             GL.End();
             window.SwapBuffers();
         }
-
     }
 }
