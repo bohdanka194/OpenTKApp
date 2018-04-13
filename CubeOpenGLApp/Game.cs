98c493b163de67ea13dcad14877ec83345a92618
bohdanka194 <bohdanka194@gmail.com>
2018-04-13 12:22:12 +0300

    draw triangle

---
+++ CubeOpenGLApp/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System.Drawing;
+
+namespace CubeOpenGLApp
+{
+    class Game
+    {
+        GameWindow window;
+        public Game(GameWindow window)
+        {
+            this.window = window;
+
+            window.Load += window_Load;
+            window.UpdateFrame += window_UpdateFrame;
+            window.RenderFrame += window_RenderFrame;
+        }
+        void window_Load(object sender, EventArgs args)
+        {
+            GL.ClearColor(Color.CornflowerBlue);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            window.SwapBuffers();
+            //throw new NotImplementedException();
+        }
+        void window_UpdateFrame(object sender, FrameEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+        void window_RenderFrame(object sender, FrameEventArgs args)
+        {
+            GL.ClearColor(Color.CornflowerBlue);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Vertex2(0, 0);
+            GL.Vertex2(0.8, 0);
+            GL.Vertex2(0, 0.8);
+            GL.End();
+            window.SwapBuffers();
+        }
+
+    }
+}
--- CubeOpenGLApp/Program.cs
+++ CubeOpenGLApp/Program.cs
@@ -10,7 +10,9 @@ namespace CubeOpenGLApp
     {
         static void Main(string[] args)
         {
-            OpenTK.GameWindow window = new OpenTK.GameWindow(800,600);
+            OpenTK.GameWindow window = new OpenTK.GameWindow(600,400);
+
+            Game game = new Game(window);
             window.Run(1.0/60.0);
         }
     }
